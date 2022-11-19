4ad5c7df7aae859ae35e158b1998c8877cf75281
joaoAaf <joao.anderson.af@gmail.com>
2022-11-19 16:56:27 -0300

    add Scanner Calculadora pedir dados em cada função.

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,4 +1,10 @@
+import java.util.Scanner;
+
 public class Calculadora {
+
+    //Pedir os dados diretamente em cada função
+    Scanner leia = new Scanner(System.in);
+
     public double soma() {
         return 0;
     }
