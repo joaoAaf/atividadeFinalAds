0c97a5835074a33da120ec182404d84bf1742e22
joaoAaf <joao.anderson.af@gmail.com>
2022-11-19 16:50:03 -0300

    Criação da classe Calculadora e declaração das funções.

---
+++ src/Calculadora.java
@@ -0,0 +1,26 @@
+public class Calculadora {
+    public double soma() {
+        return 0;
+    }
+    public double subtracao() {
+        return 0;
+    }
+    public double divisao() {
+        return 0;
+    }
+    public double multiplicacao() {
+        return 0;
+    }
+    public double exponencial() {
+        return 0;
+    }
+    public double raizQuadrada() {
+        return 0;
+    }
+    public double piso() {
+        return 0;
+    }
+    public double teto() {
+        return 0;
+    }
+}
