a2625bf8824f94723c94acba229164b15dd49984
joaoAaf <joao.anderson.af@gmail.com>
2022-11-20 17:39:12 -0300

    teste classe CalculaMateriais.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //teste classe CalculaMateriais
+        CalculaMateriais alvenaria = new CalculaMateriais();
+
+        alvenaria.materiaisParedeAlvenaria();
     }
 }
