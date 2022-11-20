81fdd0708b5e5e6ed1206c08e72d6b4775e36607
joaoAaf <joao.anderson.af@gmail.com>
2022-11-20 10:21:21 -0300

    adição da função recebeNumero

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -5,6 +5,23 @@ public class Calculadora {
     //Pedir os dados diretamente em cada função
     Scanner leia = new Scanner(System.in);
 
+    public double recebeNumeros() {
+        double numeroDouble;
+        while (true) {
+            System.out.println("Digite um número:");
+            String numero = leia.nextLine();
+            try {
+                numeroDouble = Double.parseDouble(numero);
+                break;
+            }
+            catch (Exception e) {
+                System.out.println("Número Inválido!");
+            }
+        }
+        
+        return numeroDouble;
+    }
+
     public double soma() {
         return 0;
     }
