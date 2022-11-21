f66e90bc63587c37a9e837acad86ae05976487ca
João Anderson <Joo Anderson@IFLN709102>
2022-11-21 16:43:10 -0300

    Método calculoJanelas pronto, aguardando teste e pequenas alterações.

--- src/App/CalculoJanelas.java
+++ src/App/CalculoJanelas.java
@@ -4,7 +4,7 @@ public class CalculoJanelas {
 	
 	Calculadora calculadora = new Calculadora();
 	
-	public void calculoJanelas() {
+	public double calculoJanelas() {
 		System.out.println("==========CALCULO QUANTIDADE DE JANELAS==========");
 		System.out.println("Digite as dimensões da(s) janela(s) que deseja instalar:");
 		System.out.println("Comprimento:");
@@ -18,6 +18,8 @@ public class CalculoJanelas {
 		double larguraAmbiente = calculadora.recebeNumero();
 		double areaJanela = calculadora.multiplicacao(comprimentoJanela, alturaJanela);
 		double areaAmbiente = calculadora.multiplicacao(comprimentoAmbiente, larguraAmbiente);
+		double areaJanelaBruta;
+		double quantJanelas = 0;
 		double opcao = 0;
 		while (opcao!=1 || opcao!=2) {
 			System.out.println("O ambiente em questão é um banheiro ou depósito?");
@@ -25,11 +27,18 @@ public class CalculoJanelas {
 			System.out.println("2 - Não;");
 			opcao = calculadora.recebeNumero();
 			if (opcao==1) {
-				
-			
+				areaJanelaBruta = calculadora.divisao(areaAmbiente, 8);
+				quantJanelas = calculadora.teto(calculadora.divisao(areaJanelaBruta, areaJanela));
+			}
+			else if (opcao==2) {
+				areaJanelaBruta = calculadora.divisao(areaAmbiente, 6);
+				quantJanelas = calculadora.teto(calculadora.divisao(areaJanelaBruta, areaJanela));
+			}
+			else {
+				System.out.println("Opção Inválida!");
 			}
 		}
-		
+		return quantJanelas;
 	}
 	
 }
