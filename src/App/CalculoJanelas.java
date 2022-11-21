194004c2ccf72257e8226804e107590f1fa3b056
João Anderson <Joo Anderson@IFLN709102>
2022-11-21 16:24:02 -0300

    Criação da classe CalculoJanelas e inicio do desenvolvimento da mesma.

---
+++ src/App/CalculoJanelas.java
@@ -0,0 +1,35 @@
+package App;
+
+public class CalculoJanelas {
+	
+	Calculadora calculadora = new Calculadora();
+	
+	public void calculoJanelas() {
+		System.out.println("==========CALCULO QUANTIDADE DE JANELAS==========");
+		System.out.println("Digite as dimensões da(s) janela(s) que deseja instalar:");
+		System.out.println("Comprimento:");
+		double comprimentoJanela = calculadora.recebeNumero();
+		System.out.println("Altura:");
+		double alturaJanela = calculadora.recebeNumero();
+		System.out.println("Digite as dimenções do ambiente que deseja instalar a(s) janela(s):");
+		System.out.println("Comprimento:");
+		double comprimentoAmbiente = calculadora.recebeNumero();
+		System.out.println("Largura:");
+		double larguraAmbiente = calculadora.recebeNumero();
+		double areaJanela = calculadora.multiplicacao(comprimentoJanela, alturaJanela);
+		double areaAmbiente = calculadora.multiplicacao(comprimentoAmbiente, larguraAmbiente);
+		double opcao = 0;
+		while (opcao!=1 || opcao!=2) {
+			System.out.println("O ambiente em questão é um banheiro ou depósito?");
+			System.out.println("1 - Sim;");
+			System.out.println("2 - Não;");
+			opcao = calculadora.recebeNumero();
+			if (opcao==1) {
+				
+			
+			}
+		}
+		
+	}
+	
+}
