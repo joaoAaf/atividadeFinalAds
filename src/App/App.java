4e1238a70fadd1e2874b334a94adef78f3b46892
joaoAaf <joao.anderson.af@gmail.com>
2022-11-22 19:59:57 -0300

    Comentario teste CalculaJanelas.

--- src/App/App.java
+++ src/App/App.java
@@ -1,12 +1,16 @@
 package App;
 public class App {
     public static void main(String[] args) throws Exception {
+        
         //teste classe CalculaMateriais
+        
         /*CalculaMateriais alvenaria = new CalculaMateriais();
 
         alvenaria.materiaisParedeAlvenaria();*/
     	
-    	CalculoJanelas janelas = new CalculoJanelas();
+    	//teste classe CalculaJanelas
+        
+        CalculoJanelas janelas = new CalculoJanelas();
     	
     	System.out.println(janelas.quantJanelas());
     	
